6921779cf7967e5eebe090688e46380166d79eb2
cgomezfigueroa <cgomezfigueroa@deloitte.com>
2023-08-23 11:03:35 -0600

    Comment component 23/08/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Comment from './components/Comment.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const reviewData = {
+    stars: 4,
+    title: "“La mejor computadora que he comprado”",
+    date: "18/09/22",
+    text: "Funciona bastante rápida y confiable, de inicio un pequeño problema con drivers, pero ya solucionado después de una visita al fabricante para su habilitación."
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Comment reviewData={reviewData} />
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Comment-styles.css';
+import '../assets/fonts.css';
+
+function Comment({ reviewData }) {
+  const Stars = 5;
+
+  return (
+    <div className="comment">
+      <div className="top">
+        <div className="stars">
+          {Array.from({ length: Stars }).map((_, index) => (
+            <span
+              key={index}
+              className={`icon-star ${index < reviewData.stars ? 'icon-star-full' : 'icon-star-empty'}`}
+            ></span>
+          ))}
+        </div>
+        <h2 className="title">{reviewData.title}</h2>
+      </div>
+      <h3 className="date">{reviewData.date}</h3>
+      <p className="text">{reviewData.text}</p>
+    </div>
+  );
+}
+
+export default Comment;
