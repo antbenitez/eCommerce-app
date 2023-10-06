d7a649a5e9372952b01cf8b77e315c93e385d059
Jose Luis Garza <jgarzaramos@deloitte.com>
2023-10-06 09:47:21 -0600

    feat: add quantity selector component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Comment from "./components/Comment.jsx";
-import RelatedProducts from "./components/RelatedProd.jsx";
-import Stars from "./components/Stars.jsx";
-import Accordion from "./components/Accordion.jsx";
-import Table from "./components/Table.jsx";
-import productDetails from "./assets/details.json";
-import QuantitySelector from "./components/QuantitySelector.jsx";
+import React from 'react';
+import Comment from './components/Comment.jsx';
+import RelatedProducts from './components/RelatedProd.jsx';
+import Stars from './components/Stars.jsx';
+import Accordion from './components/Accordion.jsx';
+import Table from './components/Table.jsx';
+import productDetails from './assets/details.json';
+import QuantitySelector from './components/QuantitySelector.jsx';
 
 function App() {
   const reviewData = {
     stars: 4,
-    title: "“La mejor computadora que he comprado”",
-    date: "18/09/22",
-    text: "Funciona bastante rápida y confiable, de inicio un pequeño problema con drivers, pero ya solucionado después de una visita al fabricante para su habilitación.",
+    title: '“La mejor computadora que he comprado”',
+    date: '18/09/22',
+    text: 'Funciona bastante rápida y confiable, de inicio un pequeño problema con drivers, pero ya solucionado después de una visita al fabricante para su habilitación.',
   };
 
   return (
