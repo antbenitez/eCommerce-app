6b3bf7add0350b37dbd60d6ae03022f51b779bec
Benitez <antbenitez@deloitte.com>
2023-11-29 09:43:58 -0600

    Added Product component and changes to PLP

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom"
-import ProductListPage from "./routes/ProductListPage";
+import ProductListPage from "./routes/ProductListPage/ProductListPage";
 
 function App() {
 
---
+++ src/routes/ProductListPage/ProductListPage.jsx
@@ -0,0 +1,29 @@
+import Header from "../../components/Header/Header.jsx";
+import Categories from "../../components/DropdownMenu/DropdownMenu.jsx"
+import RelatedProducts from "../../components/RelatedProd/RelatedProd.jsx";
+import Product from "../../components/Product/Product.jsx";
+import productsData from "../../assets/products.json";
+import "./ProductListPage.css"
+
+
+function ProductListPage() {
+    return (
+        <div>
+            <Header user={{name: "Pedrito"}} checkout={{itemsCount: 2}}/>
+            <div id='content'>
+            {productsData.map(({ id, image, name, price, discount, score }) => (
+                <Product
+                    id={id}
+                    name={name}
+                    price={price}
+                    discount={discount}
+                    score={score}
+                    image={image}
+                ></Product>
+            ))}
+            </div>
+        </div>
+    );
+  }
+  
+  export default ProductListPage;
